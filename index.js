0948e39c66d1343de253f5c7b8b597f303b5d59d
gesandu15 <useg329@gmail.com>
2025-08-22 07:04:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require("./pair");
+
+const pairRoute = require("./routes/pair");
 require("events").EventEmitter.defaultMaxListeners = 500;
-app.use("/code", code);
+
+app.use("/pair", pairRoute);
 
 app.use("/", async (req, res, next) => {
   res.sendFile(__path + "/pair.html");
@@ -13,8 +15,9 @@ app.use("/", async (req, res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
+  console.log(`🚀 M.R.Gesa Bot Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
